eface16b7bdb0fcacb47b07462db14b9cb5cf290
liu sai <961931945@qq.com>
2017-03-26 21:48:09 +0800

    parentNode,offsetParent,offsetTop,offsetWidth,createElement

--- javascript/妙味课堂/javascript初级/JS9-定时器管理、函数封装.html/shakeFn.js
+++ javascript/妙味课堂/javascript初级/JS9-定时器管理、函数封装.html/shakeFn.js
@@ -2,13 +2,13 @@ function shakeFn(){
 
 }
 function shake(obj,attr,dir,endFn){
-	var pos=parseInt(getStyle(obj,attr));
-	var arr=[];
-	var num=0;
-	var timer=null;
-	for (var i = 0; i < dir; i-=2) {
-		arr.push(i,-i);
-	}
+		var pos=parseInt(getStyle(obj,attr));
+		var arr=[];
+	var  num=0;
+	var     timer=null;
+				for (var i = 0; i < dir; i-=2) {
+					arr.push(i,-i);
+				}
 	arr.push(0);
 	clearInterval(timer);
 	timer=setInterval(function(){
